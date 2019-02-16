4876726740d303542ebaabfdd47b0115440538f3
Simon B-G <simonbrucegardner@gmail.com>
2019-02-16 20:23:54 +0000

    Adds file mover function

--- etl_functions.py
+++ etl_functions.py
@@ -6,12 +6,38 @@ import os
 import sqlite3
 from sqlite3 import Error
 
-def list_and_compare_files(path):
+def list_and_compare_files(to_load_path,loaded_path):
     """
     Function to compare .csvs in loading directory and
     db imports table
     """
-    files = os.listdir(path)
+
+    # list to be returned of files to load
+    loading_files = []
+
+    # lists files to be loaded
+    to_load_files = os.listdir(to_load_path)
+
+    # lists loaded files
+    loaded_files = os.listdir(loaded_path)
+
+    # Checks for files that have already been loaded in to_load_path
+    # This is in case a duplicate file is dropped
+    for file in to_load_files:
+        if file not in loaded_files:
+            loading_files.append(file)
+
+    return loading_files
+
+def move_to_loaded(loading_files):
+    """
+    Function moves loaded files to the
+    files_to_load/Loaded directory
+    """
+
+    for file in loading_files:
+        os.rename('file')
+
 
 
 def create_connection(db_file):
